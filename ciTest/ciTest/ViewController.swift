c932a8e26775f4b20aa1f864228ee5569aece428
henry9610 <henry9610@163.com>
2024-03-12 16:47:16 +0800

    init

---
+++ ciTest/ciTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ciTest
+//
+//  Created by henry on 2024/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
